a81bca53ad4bc2d976ac285f5b1f39c30180da2b
BibeshT_TXST <xmp19@txstate.edu>
2026-01-12 10:17:49 -0600

    defined Loginpage function

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthContainer } from './AuthContainer';
 import { BrandHeader } from './Header';
 import { Input } from './Input';
 import { PrimaryButton } from './Button';
+import { useAuth } from '../context/AuthContext';
+import api from '../api/axios';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { login } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //Prevents page refresh
+    setLoading(true);
+
+    try {
+      const response = await api.post('/auth/login', { email, password });    // Send  Creditials to the backend
+      const { token } = response.data;                                        // Extract JWT token from response
+      login(token);                                                           // Store the token in context
+      console.log('Login successful');
+    } catch (error) {
+      console.error('Invalid credentials, please try again:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <AuthContainer>
       <BrandHeader title="Welcome Back" subtitle="Please enter your details" />

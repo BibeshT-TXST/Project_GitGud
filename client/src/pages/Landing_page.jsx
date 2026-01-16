59aaeca54cd937d0e54a798eda6fbe6898d2ae56
BibeshT_TXST <xmp19@txstate.edu>
2026-01-16 14:52:46 -0600

    setup custom theme configuration

--- client/src/pages/Landing_page.jsx
+++ client/src/pages/Landing_page.jsx
@@ -35,3 +35,19 @@ const NAVIGATION = [
         icon: AccountCircleIcon,
     },
 ];
+
+const demoTheme = createTheme({
+    cssVariables: {
+        colorSchemeSelector: 'data-toolpad-color-scheme',
+    },
+    colorSchemes: { light: true, dark: true },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
+});

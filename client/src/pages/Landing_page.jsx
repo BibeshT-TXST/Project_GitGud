77502c988e3f05b1a04ec8ded200da64fb58660d
BibeshT_TXST <xmp19@txstate.edu>
2026-01-16 14:45:19 -0600

    Defined  navigation schema

--- client/src/pages/Landing_page.jsx
+++ client/src/pages/Landing_page.jsx
@@ -4,6 +4,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { createTheme } from '@mui/material/styles';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import DescriptionIcon from '@mui/icons-material/Description';
@@ -11,3 +13,25 @@ import LayersIcon from '@mui/icons-material/Layers';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { DemoProvider, useDemoRouter } from '@toolpad/core/internal';
+
+const NAVIGATION = [
+    {
+        kind: 'header',
+        title: 'Menu',
+    },
+    {
+        segment: 'dashboard',
+        title: 'Dashboard',
+        icon: DashboardIcon,
+    },
+    {
+        segment: 'inventory',
+        title: 'Inventory List',
+        icon: InventoryIcon,
+    },
+    {
+        segment: 'account',
+        title: 'My Account',
+        icon: AccountCircleIcon,
+    },
+];

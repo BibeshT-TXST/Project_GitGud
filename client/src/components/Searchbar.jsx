18a5f6624c3a4ace075f520cc3c05a0074b955af
BibeshT_TXST <xmp19@txstate.edu>
2026-01-21 14:50:43 -0600

    Using Autocomplete component with props to code

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,3 +1,37 @@
 import react from 'react';
 import { Autocomplete, TextFiled, InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+
+const Searchbar = ({ options, label = "Search..." }) => {
+    return (
+        <Autocomplete
+            /*Props*/
+            //Prop that allows typing beyond the listed options
+            freesolo
+            disableClearable = {false}
+            options = {options}
+            //Will be adjusted later based on data type
+            getOptionLabel = {(option) => option.label || ""}
+
+            //The text field rendering
+            renderInput = {(params) => (
+                <TextFiled
+                    {...params}
+                    label={label}
+                    variant="outlined"
+                    InputProps={{
+                        ...params.InputProps,
+                        type: 'search',
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon color="action" />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+            )}
+        />
+    );
+}
+
+export default Searchbar;

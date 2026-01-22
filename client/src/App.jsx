2d52fde94e4a532077bad7d489ba610fa3501899
BibeshT_TXST <xmp19@txstate.edu>
2026-01-22 13:10:43 -0600

    Removed extra browser wrapper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import LoginPage from './components/Login';
 import LandingPage from './pages/Landing_page'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/landing" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/landing" element={<LandingPage />} />
+    </Routes>
   );
 }
 

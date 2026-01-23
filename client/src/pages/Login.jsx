dc4065b51e567dbdf44155bdb79ac24fd12206af
BibeshT_TXST <xmp19@txstate.edu>
2026-01-23 15:18:04 -0600

    Added the top row, logo for application

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Topography from '@mui/material/Typography';
 import TokenIcon from '@mui/icons-material/Token';
+import myLogo from '../components/logo.png';
 
 const Login = () => {
     // State for form inputs
@@ -28,7 +29,15 @@ const Login = () => {
     };
 
     return (
-        
+        // Main container
+        <div className = "min-h-screen flex flex-col">
+            {/* --- Top Row: Logo Section --- */}
+            <header className="w-full p-6 flex items-center shadow-md bg-[#363534] z-20">
+                <div className="flex items-center gap-3">
+                    <img src={myLogo} alt="Inventory Logo" className="h-15 w-auto object-contain" />
+                </div>
+            </header>
+        </div>
     );
 };
 

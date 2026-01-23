26abda22956902aed9257dfb3d6474c01f87906f
BibeshT_TXST <xmp19@txstate.edu>
2026-01-23 14:27:45 -0600

    Defining states for form inputs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Topography from '@mui/material/Typography';
 import TokenIcon from '@mui/icons-material/Token';
+
+const Login = () => {
+    // State for form inputs
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
+}

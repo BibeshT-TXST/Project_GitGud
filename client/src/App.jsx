a7f0915dd9da63e5b242c87a1af5b70af4e36c30
BibeshT_TXST <xmp19@txstate.edu>
2026-01-27 13:12:40 -0600

    Adding the routing logic back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
-import LoginPage from './components/Login';
 import LandingPage from './pages/Landing_page'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.jsx';
 
 function App() {
   return (
-    <Login />
-    //<Routes>
-     //<Route path="/" element={<LoginPage />} />
-      //<Route path="/landing" element={<LandingPage />} />
-    //</Routes>
+    <Routes>
+    <Route path="/" element={<Login />} />
+      <Route path="/landing" element={<LandingPage />} />
+    </Routes>
   );
 }
 

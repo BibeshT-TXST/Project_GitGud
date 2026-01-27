fca813f1ff18757a17f38888a4797dc6848bcfe9
BibeshT_TXST <xmp19@txstate.edu>
2026-01-27 13:18:56 -0600

    Removed previous login page

--- client/src/components/AuthContainer.jsx
+++
@@ -1,7 +0,0 @@
-export const AuthContainer = ({ children }) => (
-  <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-    <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-lg">
-      {children}
-    </div>
-  </div>
-);

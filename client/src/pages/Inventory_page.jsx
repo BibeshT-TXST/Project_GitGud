7470de16bf49c95a76a8977f82653dc86288f448
BibeshT_TXST <xmp19@txstate.edu>
2026-01-30 12:36:54 -0600

    Importing required components from MUI library, initiating function Inventorypage

--- client/src/pages/Inventory_page.jsx
+++ client/src/pages/Inventory_page.jsx
@@ -1,1 +1,14 @@
-import * as React from 'react';
+import React from 'react';
+import { Box } from '@mui/material';
+import { Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
+
+/* Inventory Page Component
+*  Curently a placeholder paper component with sample text
+*/
+
+function InventoryPage() {
+
+}
+
+export default InventoryPage;

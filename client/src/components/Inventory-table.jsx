784c0e5c96131dfb81319cc86660696dc9381eb8
BibeshT_TXST <xmp19@txstate.edu>
2026-01-30 15:34:48 -0600

    changing field type of ISBN from number to string

--- client/src/components/Inventory-table.jsx
+++ client/src/components/Inventory-table.jsx
@@ -2,12 +2,7 @@ import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 
 const columns = [
-    { 
-        field: 'isbn',
-        headerName: 'ISBN',
-        type : 'number',
-        width : 100
-    },
+    { field: 'isbn', headerName: 'ISBN', width : 100},
     { field : 'title', headerName: 'Title', width: 130},
     { field : 'booktype', headerName: 'Book Type', width: 130},
     { field : 'status', headerName: 'Status', width: 130},

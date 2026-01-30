a945977aa28053af7cc3991d1789151a11645234
BibeshT_TXST <xmp19@txstate.edu>
2026-01-30 14:35:37 -0600

    Added column attributes

--- client/src/components/Inventory-table.jsx
+++ client/src/components/Inventory-table.jsx
@@ -1,2 +1,15 @@
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+
+const columns = [
+    { 
+        field: 'isbn',
+        headerName: 'ISBN',
+        type : 'number',
+        width : 100
+    },
+    { field : 'title', headerName: 'Title', width: 130},
+    { field : 'booktype', headerName: 'Book Type', width: 130},
+    { field : 'status', headerName: 'Status', width: 130},
+    { field : 'purchasedate', headerName: 'Purchase Date', width: 130},
+];

ab4bc8e8d6a8bd07e6052fbbac118de72d4f6e53
BibeshT_TXST <xmp19@txstate.edu>
2026-02-05 15:57:30 -0600

    Importing the pool object

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const pool = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;

c8c9bcaf64f588cfb2cac1232f837090edd62d7b
BibeshT_TXST <xmp19@txstate.edu>
2026-02-11 16:12:32 -0600

    Added onClick event handler to update button, the text in the button is now  conditional to edit mode state

--- client/src/pages/Inventory_page.jsx
+++ client/src/pages/Inventory_page.jsx
@@ -136,8 +136,8 @@ function InventoryPage() {
                         <Button variant="contained" onClick={() => setOpen(true)} sx={{ pr: 2, width: '100%', maxWidth: 150 }}>
                             Add Book
                         </Button>
-                        <Button variant="contained" sx={{ pr: 2, width: '100%', maxWidth: 150 }}>
-                            Update Book
+                        <Button variant="contained" sx={{ pr: 2, width: '100%', maxWidth: 150 }} onClick={handleUpdateClick}>
+                            {isEditMode ? 'Save Changes' : 'Update Book'}
                         </Button>
                     </Stack>
                     <Searchbar onSearchChange={setSearchQuery} options={searchOptions} />

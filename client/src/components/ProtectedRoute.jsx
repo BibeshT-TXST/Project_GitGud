0f8878aa9d616775deb16d589f3e3e4bcdedf616
BibeshT_TXST <xmp19@txstate.edu>
2026-02-17 16:21:46 -0600

    Fixed path issue

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from '../context/AuthContext';
 
 const ProtectedRoute = () => {
     const { token } = useAuth();

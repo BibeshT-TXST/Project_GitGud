76c35f84b8884729d6921bc2406ff99748ce98fa
BibeshT_TXST <xmp19@txstate.edu>
2026-02-17 16:05:06 -0600

    Adding boot back to logic when token doesnt exist

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,7 +2,15 @@ import { Naviage, Outlet } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
 const ProtectedRoute = () => {
+    const { token } = AuthProvider();
 
+    //If token doesnt exist, boot back to login page
+    if(!token){
+        return <Navigate to="/login" replace />
+    }
+
+    //If token exists, render the landing page
+    return <Outlet />
 };
 
 export default ProtectedRoute;

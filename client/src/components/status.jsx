5eae943561e3d8124aefb6167ec3a5ba1cf57a11
BibeshT_TXST <xmp19@txstate.edu>
2026-02-18 15:17:51 -0600

    Initializing the gegeral structure of the component

--- client/src/components/status.jsx
+++ client/src/components/status.jsx
@@ -5,3 +5,31 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import api from '../api/axios';
+
+export default function BookStatus() {
+  const [bookstatus, setBookstatus] = React.useState('');
+
+  const handleChange = (event) => {
+    setBookstatus(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={Status}
+          label="Status"
+          onChange={handleChange}
+        >
+            <MenuItem value="Out on Loan">Out on Loan</MenuItem>
+            <MenuItem value="Reserved">Reserved</MenuItem>
+            <MenuItem value="in-Repair">In-Repair</MenuItem>
+            <MenuItem value="Archived">Archived</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}

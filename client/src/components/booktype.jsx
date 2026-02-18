bda75f84f71558fd117fe00b0777fd7a2704c6af
BibeshT_TXST <xmp19@txstate.edu>
2026-02-18 15:35:52 -0600

    Establishing basic structure of book type component

--- client/src/components/booktype.jsx
+++ client/src/components/booktype.jsx
@@ -5,3 +5,30 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import api from '../api/axios';
+
+export default function Booktype() {
+  const [booktype, setBooktype] = React.useState('');
+
+  const handleChange = (event) => {
+    setBooktype(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">BookType</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={BookType}
+          label="BookType"
+          onChange={handleChange}
+        >
+            <MenuItem value="Paperback">Paperback</MenuItem>
+            <MenuItem value="HardCover">HardCover</MenuItem>
+            <MenuItem value="E-Book">E-Book</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}

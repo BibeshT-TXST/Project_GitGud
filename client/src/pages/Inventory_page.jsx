73526aad4f4df852712f4f965783904817944486
BibeshT_TXST <xmp19@txstate.edu>
2026-02-19 12:29:35 -0600

    Adding logic that triggres the download

--- client/src/pages/Inventory_page.jsx
+++ client/src/pages/Inventory_page.jsx
@@ -152,6 +152,14 @@ function InventoryPage() {
         const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;'});
         const url = URL.createObjectURL(blob);
 
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'inventory.csv');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+
     }
 
     //This constant takes rows extracted from the databaase and filters it using the text input in search bar

3a8a92a4948aa9b76201a22f2759ae8cf27c8f0c
BibeshT_TXST <xmp19@txstate.edu>
2026-02-20 15:08:34 -0600

    Adding necessary imports

---
+++ client/src/pages/Accounts.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useAuth } from '../context/AuthContext';

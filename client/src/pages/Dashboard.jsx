2b1ff220cd238e46ae785489f6cc0ac0ae887503
BibeshT_TXST <xmp19@txstate.edu>
2026-02-20 15:59:31 -0600

    Setting up default structure of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -8,6 +8,26 @@ import { useAuth } from '../context/AuthContext';
 
 /*Dashboard Page Component */
 function DashboardPage(){
+    return (
+         <Box
+            sx={{
+                width: '100%',
+                height: '100%',
+                padding: 3,
+            }}
+        >
+            <Card
+                sx={{
+                    maxWidth: 450,
+                    mx: 'auto',
+                    mt: 4,
+                    borderRadius: 3,
+                }}
+            >
+
+            </Card>
+        </Box>    
+    )
 
 }
 

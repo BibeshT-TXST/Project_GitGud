dc931b730d250dd6db05771c1ac89d34537f8ca4
BibeshT_TXST <xmp19@txstate.edu>
2026-02-20 15:13:20 -0600

    Settings up skeletal structure of Accounts page, pulling users NetID from auth context

--- client/src/pages/Accounts.jsx
+++ client/src/pages/Accounts.jsx
@@ -5,3 +5,13 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuth } from '../context/AuthContext';
+
+/* Accounts Page Component */
+function AccountsPage() {
+
+    /* Pull the users NetID from auth context */
+    const { user } = useAuth();
+
+}
+
+export default AccountsPage;

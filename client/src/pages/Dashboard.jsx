9519d9843be632156541dbddd05deb86d7065db1
BibeshT_TXST <xmp19@txstate.edu>
2026-02-23 16:23:03 -0600

    Editing relevant imports for dasboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { useAuth } from '../context/AuthContext';
+import api from '../api/axios';
+import BookStats from '../components/BookStats';
 
 /*Dashboard Page Component */
 function DashboardPage(){

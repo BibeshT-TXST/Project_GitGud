52a94730b3f38411fc765498a719a1ce9ec236c4
BibeshT_TXST <xmp19@txstate.edu>
2026-02-27 13:45:55 -0600

    Adding necessary imports

--- client/src/pages/Successful_logout.jsx
+++ client/src/pages/Successful_logout.jsx
@@ -1,1 +1,5 @@
-/* This page will only be displayed when successfully logging out */
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Typography } from '@mui/material';
+import { useAuth } from '../context/AuthContext';
+import api from '../api/axios';

5da317ddbee97bc0447871cf088f9d0bce7f01af
BibeshT_TXST <xmp19@txstate.edu>
2026-02-27 09:48:59 -0600

    Adding relevant imports to the logout component

---
+++ client/src/components/LogoutModal.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Modal,
+  Card,
+  CardContent,
+  TextField,
+  MenuItem,
+  Stack,
+  Typography,
+  Backdrop,
+  Fade,
+  createTheme,
+  ThemeProvider
+} from '@mui/material';

04b57474da1c431f4755482e3cdd8128edc1becf
BibeshT_TXST <xmp19@txstate.edu>
2026-02-27 14:10:57 -0600

    Adding routing logic to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import LandingPage from './pages/Landing_page'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
+import SuccessfulLogout from './pages/Successful_logout.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/successful-logout" element={<SuccessfulLogout />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/landing/*" element={<LandingPage />} />
       </Route>

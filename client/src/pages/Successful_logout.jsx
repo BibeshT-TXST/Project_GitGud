74ddcce008f13b2c1b80fb82c6b6494ccb1d0ba8
BibeshT_TXST <xmp19@txstate.edu>
2026-02-27 13:51:17 -0600

    Defining the function

--- client/src/pages/Successful_logout.jsx
+++ client/src/pages/Successful_logout.jsx
@@ -3,3 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import { Box, Typography } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
 import api from '../api/axios';
+
+export default function SuccessfulLogout() {
+    const [seconds, setSeconds] = useState(12);
+    const navigate = useNavigate();
+    const { logout } = useAuth(); // Needed to clear context
+
+    return (
+        <Box sx={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography variant="h3" gutterBottom>Successfully Logged out</Typography>
+            <Typography variant="h6">Redirecting to the login page in {seconds} seconds...</Typography>
+        </Box>
+    );
+}
